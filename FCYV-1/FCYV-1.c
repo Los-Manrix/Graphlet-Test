ce53886997240b77591fb8096428fae388ccd656
matgreco <m.grecochandia@uandresbello.edu>
2026-04-14 13:09:40 -0400

    bug arreglado era un iter que no tenia el ++

--- FCYV-1/FCYV-1.c
+++ FCYV-1/FCYV-1.c
@@ -4,6 +4,12 @@
 
 #define MAX_NODES 50000
 #define MAX_QUEUE 50000
+#define DEBUG 0
+#define VERBOSE 0
+#define DBG_PAUSE if(DEBUG) getchar()
+#define VRB_PRINT(...) if(VERBOSE) printf(__VA_ARGS__)
+
+unsigned long long int nexpansions = 0; 
 
 struct snode;
 typedef struct snode snode;
@@ -15,7 +21,6 @@ struct snode //search nodes
   unsigned iter;
   unsigned nsuccs;
   short tpc;
-  short depth;
   snode *parent;
 };
 
@@ -89,11 +94,11 @@ void printGraph(struct Graph* graph) {
     int i;
     struct AdjListNode* cur;
 	for (i = 0; i < graph->V; i++) {
-        printf("%d: nsuccs:%d", i,searchNodes[i]->nsuccs);
+        VRB_PRINT("%d: nsuccs:%d", i,searchNodes[i]->nsuccs);
         for (cur = graph->array[i]; cur; cur = cur->next) {
-            printf(" [%d %d]", cur->dest,cur->type);
+            VRB_PRINT(" [%d %d]", cur->dest,cur->type);
         }
-        printf("\n");
+        VRB_PRINT("\n");
     }
 }
 
@@ -104,7 +109,6 @@ snode* new_SearchNode(int id) {
   	node->iter = 0;
   	node->tpc = 0;
   	node->nsuccs = 0;
-  	node->depth = 0;
 	node->parent = NULL;
     return node;
 }
@@ -128,11 +132,15 @@ void ReadGraph(const char* filename) {
 	fscanf(f, "%d %d", &num_gnodes, &num_arcs);
 	fscanf(f, "\n");
 	printf("%d %d %d\n", num_gnodes, num_arcs,INT_MAX);
-	getchar();
+	DBG_PAUSE;
 	graph = createGraph(num_gnodes);
 	initializeSearchNodes();
 	for (i = 0; i < num_arcs; i++) {
+
 		fscanf(f, "%d %d %d\n", &ori, &dest, &t);
+		if(ori-1==dest-1){
+			continue;
+		}
 		addEdge(graph, ori-1, dest-1, t);
 		searchNodes[ori-1]->nsuccs++;
 	//	printf("%d %d %d %d\n", ori, dest, dist, t);
@@ -152,10 +160,14 @@ void initialize_type(){
 
 void print_types(){
 	int i,j,k;
+	long long int total = 0;
 	for(i = 0;i < 4;i++)
 		for(j = 0;j < 4;j++)
-			for(k = 0;k < 4;k++)
-				printf("[%d][%d][%d] : %d\n",i,j,k,type[i][j][k]);
+			for(k = 0;k < 4;k++){
+				printf("[%d][%d][%d] : %lld\n",i,j,k,type[i][j][k]);
+				total += type[i][j][k]; //< 0 ? -type[i][j][k] : type[i][j][k];
+			}
+	printf("Total subgrafos: %lld\n", total);
 }
 
 
@@ -170,24 +182,22 @@ long long comb2(int n) {
 
 
 void search_motif(snode* node, int iter){
-	int n1 = 0, n2 = 0, n3 = 0;
+	int n1 = 0, n2 = 0, n3 = 0; //n1 succ, n2 pred, n3 ambos
 	node->parent = NULL;
 	node->color = 1; //1 is red
-	node->depth = 0;
 	struct AdjListNode* cur;
 	//printf("\n current node %d\n", node->id);
 
 	//printf("antes n1:%d comb2(n1):%d n2:%d n3:%d - %d %d %d %d %d %d\n",n1,comb2(n1),n2,n3,type[1][0][1],type[2][0][2],type[3][0][3],type[1][0][2],type[1][0][3],type[2][0][3]);		
 	    
-	for (cur = graph->array[node->id]; cur; cur = cur->next) {
-		// Optimización: solo procesar sucesores con ID mayor (procesamiento direccional)
-		if (cur->dest <= node->id) continue;
-		
+	for (cur = graph->array[node->id]; cur; cur = cur->next) {     
 	//		printf(" [%d %d]", cur->dest,cur->type);
+		
 		snode* succ = searchNodes[cur->dest]; 
+		
 		if (succ->color != 1){
+			nexpansions++;
 			succ->tpc = cur->type;
-			succ->depth = 1; // Profundidad 1 desde el nodo raíz
 			if (succ->tpc == 1)
 				n1++;
 			if (succ->tpc == 2)
@@ -199,54 +209,42 @@ void search_motif(snode* node, int iter){
 			//printf("Insert node %d\n", succ->id);
 		}				
     }
-	type[1][0][1] += comb2(n1);
-	type[2][0][2] += comb2(n2);
-	type[3][0][3] += comb2(n3);
-	type[1][0][2] += comb2(n1 + n2) - comb2(n1) - comb2(n2);
-	type[1][0][3] += comb2(n1 + n3) - comb2(n1) - comb2(n3);
-	type[2][0][3] += comb2(n2 + n3) - comb2(n2) - comb2(n3);
-    if (type[1][0][1] > INT_MAX){
-		printf("llego %d\n",INT_MAX);
+	type[1][0][1] += comb2(n1); //1 
+	type[2][0][2] += comb2(n2); //0 
+	type[3][0][3] += comb2(n3); //0
+	type[1][0][2] += comb2(n1 + n2) - comb2(n1) - comb2(n2); //1 - 1 - 0 = 0
+	type[1][0][3] += comb2(n1 + n3) - comb2(n1) - comb2(n3); //3 - 1 - 0 = 2
+	type[2][0][3] += comb2(n2 + n3) - comb2(n2) - comb2(n3); //0 - 0 - 0 = 0
+	if (type[1][0][1] > INT_MAX){
+		VRB_PRINT("llego %d\n",INT_MAX);
 //	getchar();
 	}
 
     if (type[1][0][1] > LLONG_MAX || type[1][0][1] < 0){
-		printf("llego %d\n",LLONG_MAX);
-	getchar();
+		VRB_PRINT("llego %lld\n",LLONG_MAX);
+		DBG_PAUSE;
 	}
-		
 
-	//printf("n1:%d comb2(n1):%d n2:%d n3:%d - %d %d %d %d %d %d\n",n1,comb2(n1),n2,n3,type[1][0][1],type[2][0][2],type[3][0][3],type[1][0][2],type[1][0][3],type[2][0][3]);		
+
+	VRB_PRINT("n1:%d comb2(n1):%lld n2:%d n3:%d - %lld %lld %lld %lld %lld %lld\n",n1,comb2(n1),n2,n3,type[1][0][1],type[2][0][2],type[3][0][3],type[1][0][2],type[1][0][3],type[2][0][3]);
+	DBG_PAUSE;
 	while (!(empty_queue())){
 		snode* s = queue_pop();
-		
-		// Optimización: marcar como procesado y evitar reprocesar
-		if (s->color == 2) continue;
-		s->color = 2; // Marcar como procesado
 		s->iter = iter;
-		
-		// Optimización: limitar profundidad a 2 (motifs de 3 nodos)
-		if (s->depth >= 2) continue;
-		
-		//printf("  Pop node %d\n", s->id);
+		VRB_PRINT("  Pop node %d\n", s->id);
 		for (cur = graph->array[s->id]; cur; cur = cur->next) {
-			// Optimización: solo procesar aristas hacia adelante
-			if (cur->dest <= node->id) continue;
-			
+
 			snode* n = searchNodes[cur->dest];
-			
-			// Optimización: pre-filtrar nodos no relevantes
-			if (n->parent == NULL) continue;
-			
 			if (n->color != 1 && n->iter != iter){
+				nexpansions++;
 				//printf("    See node %d\n", n->id);
 				if (n->parent == s->parent){
-					//printf("Se resta 1 a [%d][%d][%d]\n",s->tpc,0,n->tpc);
-					//printf("Se agrega 1 a [%d][%d][%d]\n",s->tpc,cur->type,n->tpc);
+					VRB_PRINT("Se resta 1 a [%d][%d][%d]\n",s->tpc,0,n->tpc);
+					VRB_PRINT("Se agrega 1 a [%d][%d][%d]\n",s->tpc,cur->type,n->tpc);
 					type[s->tpc][0][n->tpc]--;
 					type[s->tpc][cur->type][n->tpc]++;
 					//if (type[s->tpc][0][n->tpc] < 0)
-					//	getchar();
+						DBG_PAUSE;
 				}else{
 					//printf("False Se agrega 1 a [%d][%d][%d]\n",s->tpc,cur->type,0);
 					type[s->tpc][cur->type][0]++;
@@ -263,15 +261,19 @@ void search_motif(snode* node, int iter){
 void search_motif_driver(){
 	int i,iter = 1;
 	struct AdjListNode* cur;
-	//printf("%d: nsuccs:%d\n", 2,searchNodes[2]->nsuccs);
 	initialize_type();
-//	search_motif(searchNodes[2], iter);
+	int maxnumbersucc = 0;
 	for (i = 0; i < graph->V; i++) {
-        printf("%d: nsuccs:%d %d\n", i,searchNodes[i]->nsuccs,type[1][0][1]);
-        search_motif(searchNodes[i], iter);
-        clear_queue();
+        VRB_PRINT("%d: nsuccs:%d %lld\n", i,searchNodes[i]->nsuccs,type[1][0][1]);
+        if (maxnumbersucc < searchNodes[i]->nsuccs)
+          maxnumbersucc = searchNodes[i]->nsuccs;
+		search_motif(searchNodes[i], iter);
+        //getchar();
+		iter++;
+		clear_queue();
 	}
 	print_types();
+	printf("maxnumbersucc:%d\n",maxnumbersucc);
 }
 /*
 for all s belong to G
@@ -279,20 +281,40 @@ for all s belong to G
 	iter++
 }*/
 
+/*
 int main() {
-
+	printf("comb2(1): %lld\n",comb2(1));
+	getchar();
+	//ReadGraph("./Benchmarks/outs/TFLink_Danio_rerio_interactions_LS_simpleFormat_v1.0_procesado.txt");
     //ReadGraph("yeast_procesado_carlos.txt");
-	//ReadGraph("graph_procesado.txt");
+	//ReadGraph("3graph_procesado.txt");
+	ReadGraph("./Investigacion-Graphlets/TOYS/FormatoP/12nodos_grafo_doble_procesado.txt");
 	//ReadGraph("TFLink_Homo_sapiens.txt");
 	//ReadGraph("TFLink_Danio_rerio_interactions_LS_simpleFormat_v1.0_procesado.txt");
 	//ReadGraph("TFLink_Rattus_norvegicus_interactions_LS_simpleFormat_v1.0_procesado.txt");
 //	ReadGraph("TFLink_Mus_musculus_interactions_LS_simpleFormat_v1.0.tsv_procesado.txt");
-	ReadGraph("TFLink_Drosophila_melanogaster_interactions_LS_simpleFormat_v1.0_procesado.txt");
+//	ReadGraph("TFLink_Drosophila_melanogaster_interactions_LS_simpleFormat_v1.0_procesado.txt");
 
 	search_motif_driver();
 	
-    printf("Adjacency list representation:\n");
+    printf("nexpansions: %llu\n", nexpansions);
    // printGraph(graph);
 
+    return 0;
+}
+*/
+int main(int argc, char *argv[]) {
+    printf("comb2(1): %lld\n", comb2(1));
+    DBG_PAUSE;
+
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <archivo_grafo>\n", argv[0]);
+        return 1;
+    }
+
+    ReadGraph(argv[1]);
+    search_motif_driver();
+
+    printf("nexpansions: %llu\n", nexpansions);
     return 0;
 }
